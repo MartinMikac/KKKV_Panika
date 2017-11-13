d39465bb060ebc111f8ac21bac0db69a52d68cff
Mikač Martin <martin.mikac@kkkv.cz>
2017-11-13 14:01:21 +0100

    work cmt

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -10,10 +11,28 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
-        return new ViewModel();
+class IndexController extends AbstractActionController {
+
+    /**
+     * Je uživatel autorizován
+     *
+     * @var $isAuthorised type
+     */
+    public $isAuthorised = false;
+
+    public function indexAction() {
+
+        // pokud není autorizován pak přesměrovat na přihlášení
+        if ($this->isAuthorised == false) {
+
+            $vm = new ViewModel();
+
+            $this->layout('layout/login');
+            return §vm;
+        } else {
+            $vm = new ViewModel();
+            return $vm;
+        }
     }
+
 }
---
+++ module/Application/src/Controller/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Description of LoginController
+ *
+ * @author martin.mikac
+ */
+class LoginController extends AbstractActionController {
+    //put your code here
+}
