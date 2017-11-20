f3d5bab7ae0c3b1708dc2471939c0776d7495c97
MartinMikac <martin.mikac@gmail.com>
2017-11-21 00:40:22 +0100

    NASTAVIT charset na UTF-8 bacha na IP

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -23,6 +23,7 @@ return [
                     'user' => 'root',
                     'password' => 'toor',
                     'dbname' => 'panika',
+                    'charset'  => 'utf8',
                 ]
             ],
         ],
