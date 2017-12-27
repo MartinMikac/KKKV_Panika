da39a89848b8099e9356ddb031b271f4ab26590c
Mikač Martin <martin.mikac@kkkv.cz>
2017-12-27 14:41:15 +0100

    cmt

--- module/Application/src/Repository/NastaveniRepository.php
+++ module/Application/src/Repository/NastaveniRepository.php
@@ -5,3 +5,35 @@
  * @ Martin Mikač - 2017  * 
  */
 
+namespace Application\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Application\Entity\Nastaveni;
+
+/**
+ * This is the custom repository class for Post entity.
+ */
+class NastaveniRepository extends EntityRepository {
+
+    /**
+     * Retrieves all published posts in descending date order.
+     * @return Query
+     */
+    public function NajdiNastaveniDleIdAdmin($id) {
+        $entityManager = $this->getEntityManager();
+
+        $queryBuilder = $entityManager->createQueryBuilder();
+
+        $queryBuilder->select('p')
+                ->from(Admin::class, 'p')
+                ->where('p.id_admins = :id_admin')
+                ->setParameter(':id_admin',$id);
+        
+       
+       
+       
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+
+}
